a5a78c20f45061674baf7ba7e68e201ab4d8eba1
dunzolo <rossidavide108@gmail.com>
2023-02-17 16:58:25 +0100

    added top footer

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,75 @@
+<footer>
+    <div class="top-footer">
+        <div class="container">
+            <div class="footer-links">
+                <div class="links">
+                    <h2>DC COMICS</h2>
+                    <nav>
+                        <ul>
+                            <li><a href="#">Characters</a></li>
+                            <li><a href="#">Comics</a></li>
+                            <li><a href="#">Movies</a></li>
+                            <li><a href="#">TV</a></li>
+                            <li><a href="#">Games</a></li>
+                            <li><a href="#">Videos</a></li>
+                            <li><a href="#">News</a></li>
+                        </ul>
+                    </nav>
+                    <h2>SHOP</h2>
+                    <nav>
+                        <ul>
+                            <li><a href="#">Shop DC</a></li>
+                            <li><a href="#">Shop DC Collectibles</a></li>
+                        </ul>
+                    </nav>
+                </div>
+                <div class="links">
+                    <h2>DC</h2>
+                    <nav>
+                        <ul>
+                            <li><a href="#">Terms Of Use</a></li>
+                            <li><a href="#">Privacy policy (New)</a></li>
+                            <li><a href="#">Ad Chois</a></li>
+                            <li><a href="#">Advertising</a></li>
+                            <li><a href="#">Jobs</a></li>
+                            <li><a href="#">Subscriptions</a></li>
+                            <li><a href="#">Talent Workshop</a></li>
+                            <li><a href="#">CPSC Cerification</a></li>
+                            <li><a href="#">Ratings</a></li>
+                            <li><a href="#">Shop Help</a></li>
+                            <li><a href="#">Contacts Us</a></li>
+
+                        </ul>
+                    </nav>
+                </div>
+                <div class="links">
+                    <h2>SITES</h2>
+                    <nav>
+                        <ul>
+                            <li><a href="#">DC</a></li>
+                            <li><a href="#">Mad Magazine</a></li>
+                            <li><a href="#">DC Kids</a></li>
+                            <li><a href="#">DC Universe</a></li>
+                            <li><a href="#">DC Power Visa</a></li>
+                        </ul>
+                    </nav>
+                </div>
+            </div>
+        </div>
+    </div>
+    {{-- <div class="bottom-footer">
+        <div class="container">
+            <a href="#" class="login">SIGN-UP NOW</a>
+            <nav>
+                <h2>FOLLOW US</h2>
+                <ul>
+                    <li v-for="(item, index) in socials" :key="index">
+                        <a href="/">
+                            <img :src="`/${item}`" alt="">
+                        </a>
+                    </li>
+                </ul>
+            </nav>
+        </div>
+    </div> --}}
+</footer>
